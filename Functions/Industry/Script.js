03c6c3531f59d4cf9b00715250c27e209e4b1093
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-09-27 18:21:46 -0400

    industry
    industry commit

--- Functions/Industry/Script.js
+++ Functions/Industry/Script.js
@@ -1,6 +1,13 @@
-// Finding out how much an item is in bulk
-// Item price *
+// Cost of supplies needed to open a restaurant
+// Item price * Price Per Bulk
 
-var height = prompt("")
-var width = prompt("")
-var length = prompt("")
+var total = restaurant(100,50,60,50,15);
+
+function supplies (grill, register, tables, food, chairs){
+    //
+    var supplies = grill + register + tables + food + chairs
+
+    return supplies
+}
+
+console.log("It will cost " + supplies + " to get your restaurant up and running")
