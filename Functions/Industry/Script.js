ad8a49936c5ce717b6875cf84596059ec9a23366
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-09-27 17:53:34 -0400

    Functions
    Functions

---
+++ Functions/Industry/Script.js
@@ -0,0 +1,6 @@
+// Finding out how much an item is in bulk
+// Item price *
+
+var height = prompt("")
+var width = prompt("")
+var length = prompt("")
---
+++ Functions/Worksheet/Functions.js
@@ -0,0 +1,31 @@
+//Circumference
+//Formula = C = d * pi
+
+function circumference (){
+
+    var Diameter = 16;
+    var pi = 3.14;
+    var answer = Diameter * pi;
+    return answer;
+}
+console.log("After doing the math the circumference of this particular circle is "+ circumference());
+
+
+//Stung Calculator
+var victimWeight = 250;
+function sting(){
+    var victimWeight = 250;
+    var stingsForDeath = 8.66666667;
+    var death = victimWeight * stingsForDeath;
+    return death;
+}
+
+console.log("It would take "+ sting() + " bee stings to kill a " + victimWeight +"lbs animal.");
+
+
+
+
+
+
+
+
